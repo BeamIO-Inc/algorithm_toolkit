4e44f120dfcfc94311eddb3221ed594d0ee253e5
Chris Willey <cwilley@gmail.com>
2021-02-09 16:45:43 -0500

    Began refactoring chains.json

--- algorithm_toolkit/utils/file_utils.py
+++ algorithm_toolkit/utils/file_utils.py
@@ -42,15 +42,22 @@ def get_algorithm(path):
 
 
 def get_chain_def(path, chain_name=None):
+    chain_path = os.path.join(path, 'chains')
     chain_defs = {}
-    try:
-        with open(os.path.join(path, 'chains.json'), 'r') as chain_file:
-            try:
-                chain_defs = json.load(chain_file)
-            except ValueError:
-                return {}
-    except IOError:
-        return {}
+
+    for root, dirs, files in os.walk(chain_path):
+        for f in files:
+            if '.json' in f:
+                temp_key = f.replace('.json', '')
+                try:
+                    with open(os.path.join(chain_path, f)) as the_file:
+                        try:
+                            temp_obj = json.load(the_file)
+                            chain_defs[temp_key] = temp_obj[temp_key]
+                        except ValueError:
+                            return {}
+                except IOError:
+                    return {}
 
     if chain_name:
         try:
@@ -59,6 +66,27 @@ def get_chain_def(path, chain_name=None):
             return {}
     else:
         return chain_defs
+
+
+def clear_chains(path):
+    chain_path = os.path.join(path, 'chains')
+
+    for root, dirs, files in os.walk(chain_path):
+        for f in files:
+            try:
+                os.unlink(os.path.join(chain_path, f))
+            except IOError:
+                pass
+
+
+def save_chain_files(path, chains):
+    for k, v in chains.items():
+        try:
+            with open(os.path.join(path, 'chains', k + '.json'), 'w') as f:
+                temp_json = {k: v}
+                json.dump(temp_json, f, indent=4, separators=(',', ': '))
+        except IOError:
+            pass
 
 
 def get_json_path(path, a):
