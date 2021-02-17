43b8c7e615e4988bf935d7e7e01cde7fc4551a45
Ryan LaClair <ryanlaclair@gmail.com>
2021-02-17 15:29:50 -0500

    Updated to leave logging to implementations rather than framework

--- algorithm_toolkit/__init__.py
+++ algorithm_toolkit/__init__.py
@@ -1,11 +1,5 @@
-import logging
-
-logging.basicConfig(format='%(asctime)s:%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
-
 store = {}
 
-# TODO fill in config
 config = {
-    'DEFAULT_WORKING_ROOT': ''
+    'DEFAULT_WORKING_ROOT': '/tmp'
 }
