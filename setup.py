5df30e9cfb30543b3a980da7eca4d4f366820f78
Ryan LaClair <ryanlaclair@gmail.com>
2021-03-01 15:35:16 -0500

    Added missing views package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "developers to develop and share algorithms related to "
         "geospatial data and imagery."
     ),
-    packages=find_packages(include=['algorithm_toolkit', 'atk', 'atk_cli']),
+    packages=find_packages(include=['algorithm_toolkit', 'atk', 'atk.views', 'atk_cli']),
     include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
